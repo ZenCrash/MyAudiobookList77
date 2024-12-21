86f0afc282af0c6a6b8a57512c8315ef2ddb4fb1
Allan <76095439+ZenCrash@users.noreply.github.com>
2024-12-21 22:56:17 +0100

    Add project files.

---
+++ MyAudiobookList77/AudiobookContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyAudiobookList.DataAccess.Models;
+
+namespace MyAudiobookList.DataAccess
+{
+  public class AudiobookContext : DbContext
+  {
+
+    public DbSet<AudioBook> AudioBooks { get; set; }
+
+    public AudiobookContext(DbContextOptions<AudiobookContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlite("Data Source=Database/MyAudiobookDb.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+    }
+  }
+}
---
+++ MyAudiobookList77/Models/Audiobook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAudiobookList.DataAccess.Models
+{
+  public class AudioBook
+  {
+    [Key]
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string? Subtitle { get; set; }
+    public string Author { get; set; }
+    public string NarratedBy { get; set; }
+    public string? Series { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string Language { get; set; }
+    public string? Ratings { get; set; }
+    public string? Stars { get; set; }
+    public string? CoverUrl { get; set; }
+    public string? BookNumber { get; set; }
+  }
+}
